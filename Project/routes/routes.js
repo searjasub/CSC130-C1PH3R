e23771a466980948cc7aacf448d066ce3dd0b047
Searjasub Lopez <slopez@student.neumont.edu>
2020-02-05 13:30:22 -0700

    Fixed UI to match website

--- Project/routes/routes.js
+++ Project/routes/routes.js
@@ -2,7 +2,28 @@ const config = require('../config');
 
 exports.home = async (req, res) => {
     res.render('home', {
-        title : 'Home',
+        title : 'C1PH3RS',
         "config" : config
     });
+};
+
+exports.game = async (req, res) => {
+    res.render('game', {
+        title : 'Games',
+        "config" : config
+    })
+};
+
+exports.ciphers = async (req, res) => {
+    res.render('ciphers', {
+        title : 'Collection of Ciphers',
+        "config" : config
+    })
+};
+
+exports.atbash = async (req, res) => {
+    res.render('atBash', {
+        title : 'Atbash Cipher',
+        "config" : config
+    })
 };
