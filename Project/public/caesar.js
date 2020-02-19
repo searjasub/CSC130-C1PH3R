acbd8de6408f6b78c73bf108ab9e2ba5beda18dc
Mariela Cisneros <47341516+MarielaCisneros@users.noreply.github.com>
2020-02-19 11:17:39 -0700

    Encript and decript

--- Project/public/caesar.js
+++ Project/public/caesar.js
@@ -1,52 +1,53 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const fullAlphabet = alphabet + alphabet + alphabet;
+const alphabet2 = "zyxwvutsrqponmlkjihgfedcba";
 
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var fullAlphabet = alphabet + alphabet + alphabet;
+const encripB = document.getElementById("encrip");
+encripB.addEventListener("click", function(evt) {
+  evt.preventDefault();
 
+  const encripChipher = document.getElementById("decipher_caesar_text").value;
+  const offset = document.getElementById("offset").value;
 
-function runCipher() {
-  var cipherText = $("#chypher").val();
-  var cipherOffset = $("#offset").val();
-  cipherOffset = (cipherOffset % alphabet.length);
-  var cipherFinish = '';
+  for(i = 0; i < encripChipher.length; i++) {
+    const letters = encripChipher[i];
+    const position = alphabet.indexOf(letters);
+    console.log("Original Position: " + position);
 
-  for(i = 0; i < cipherText.length; i++) {
-     var letter = cipherText[i];
-     var upper = (letter == letter.toUpperCase());
-     letter = letter.toLowerCase();
-    
-     var index = alphabet.indexOf(letter);
-     if(index == -1) {
-       cipherFinish += letter;
-     } else {
-       index = ((index + cipherOffset) + alphabet.length);
-       var nextLetter = fullAlphabet[index];
-       if(upper) {
-        nextLetter = nextLetter.toUpperCase();
-        cipherFinish += nextLetter;
-       } 
-     }
-  }
-    
-  $("#finish").val(cipherFinish);
-}
+    if(position == -1) {
+      document.getElementById("bottom-cipher").value += letters;
+    } else {
+      const nextPosition = position + offset % 26;
+      console.log("Position afetr shift: " + nextPosition);
+      const nextLetter = fullAlphabet[nextPosition];
 
-$(document).ready(function() {
-  $('#cypher').keypress(function(){
-    setTimeout(function(){ runCipher(); },20);
-  });
-  $('#cypher').blur(function(){
-    runCipher();
-  });
-  $('#offset').change(function(){
-    setTimeout(runCipher(),20);
-  });
-  
+      document.getElementById("bottom-cipher").value += nextLetter;
+    }
+   }
 });
 
-// const helpButt = document.getElementById("help");
-// helpButt.addEventListener("click", function(evt) {
-//     evt.preventDefault();
-//     const text = "The Caesar cipher is one of the earliest known and simplest ciphers. It is a type of substitution cipher in which each letter in the plaintext is 'shifted' a certain number of places down the alphabet. For example, with a shift of 1, A would be replaced by B, B would become C, and so on. The method is named after Julius Caesar, who apparently used it to communicate with his generals.";
+const decryptB = document.getElementById('decrypt');
+decryptB.addEventListener("click", function(evt) {
+  evt.preventDefault();
 
-    
-// });
+  const decryptCipher = document.getElementById("bottom-cipher").value;
+  const set = document.getElementById("offset").value;
+
+  for(i = 0; i < decryptCipher.length; i++) {
+    const letter = decryptCipher[i];
+    const index = alphabet.indexOf(letter);
+    console.log("Original Position: " + index);
+  
+    if(index <= -1) {
+      document.getElementById("decipher_caesar_text").value += letter;
+    } else {
+      const nextIndex = index - set % 26;
+      console.log("New Position: " + nextIndex);
+      const nextLetters = fullAlphabet[nextIndex];
+      document.getElementById("decipher_caesar_text").value += nextLetters;
+    }
+  }
+});
+
+console.log(encripB)
+console.log(decryptB)
