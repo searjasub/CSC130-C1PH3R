658ecd89b8e6a610f013d565a754c0efdda24359
Zane Slocum <zwslocum@yahoo.com>
2020-02-24 17:46:48 -0700

    Add basic game
    Add basic game logic. Planned for additional touches to make the game more interesting/challenging

---
+++ Project/public/game.js
@@ -0,0 +1,31 @@
+var answer;
+var score;
+var currentQuestion;
+var questions = new Map([
+    ["dGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw==", "Base64"],
+    ["gsv jfrxp yildm ulc qfnkh levi gsv ozab wlt", "Atbash"],
+    ["god yzal eht revo spmuj xof nworb kciuq eht", "Rotate"],
+    ["- .... . / --.- ..- .. -.-. -.- / -... .-. --- .-- -. / ..-. --- -..- / .--- ..- -- .--. ... / --- ...- . .-. / - .... . / .-.. .- --.. -.-- / -.. --- --.", "Morse"],
+    ["gur dhvpx oebja sbk whzcf bire gur ynml qbt", "ROT13"]
+]);
+var keys = Array.from(questions.keys());
+
+function start() {
+    score = 0;
+    changeQuestion();
+}
+
+function changeQuestion() {
+    document.getElementById("score").innerHTML = score;
+    currentQuestion = keys[Math.floor(Math.random() * Math.floor(4))];
+    document.getElementById("question text").innerHTML = currentQuestion;
+    answer = questions.get(currentQuestion);
+}
+
+function guess() {
+    str = document.getElementById("guessInput").value;
+    if (str === answer) {
+        score++;
+    }
+    changeQuestion();
+}
